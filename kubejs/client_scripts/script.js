acf3d46e0f6fbf438e0d070f8ea1a5f6eeb37785
alexander-neumeier <63714415+alexander-neumeier@users.noreply.github.com>
2022-11-23 18:59:39 +0100

    Update script.js

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -3,6 +3,8 @@
 console.info('Hello, World! (You will see this line every time client resources reload)')
 
 onEvent('jei.hide.items', event => {
+	event.hide('mekanism:creative_fluid_tank')
+	event.hide('ftblibrary:fluid_container')
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
 })
