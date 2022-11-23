f081f04901ec83af82daf684d89879b5daa8683b
alexander-neumeier <63714415+alexander-neumeier@users.noreply.github.com>
2022-11-23 20:33:09 +0100

    Update script.js

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -2,12 +2,7 @@
 
 console.info('Hello, World! (You will only see this line once in console, during startup)')
 
-onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-})
 
 onEvent('block.registry', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+  event.create('xnet_frame').model('xnet_frame').displayName('Xnet Machine Frame').material('stone').requiresTool(false)
 })
